0d78cc8762804ab065006d7688c159abc98a1199
SwarnimRaj007 <swarnimarmy@gmail.com>
2026-04-09 23:48:01 +0530

    Refactor BloodInventoryModule to use separate maps

--- BloodInventoryModule.java
+++ BloodInventoryModule.java
@@ -1,50 +1,63 @@
-import java.util.*;import java.io.*;
+import java.io.*;
+import java.util.*;
 
-class BloodInventoryModule{
-static HashMap<String,int[]>map=new HashMap<>();
-static{String[]g={"A+","A-","B+","B-","O+","O-","AB+","AB-"};
-for(String s:g)map.put(s,new int[]{0,0,0});load();}
-
-static void add(String bg){
-int[]a=map.get(bg);a[0]++;a[1]++;a[2]++;save();
+public class BloodInventoryModule{
+static HashMap<String,Integer>rbc=new HashMap<>();
+static HashMap<String,Integer>plasma=new HashMap<>();
+static HashMap<String,Integer>platelets=new HashMap<>();
+static String[]groups={"A+","A-","B+","B-","O+","O-","AB+","AB-"};
+static void init(){
+for(String g:groups){
+rbc.put(g,0);
+plasma.put(g,0);
+platelets.put(g,0);
 }
-
-static boolean check(String bg,int t,int u){
-return map.get(bg)[t]>=u;
+load();
 }
-
-static void deduct(String bg,int t,int u){
-map.get(bg)[t]-=u;save();
+static void add(String g){
+rbc.put(g,rbc.get(g)+1);
+plasma.put(g,plasma.get(g)+1);
+platelets.put(g,platelets.get(g)+1);
+save();
 }
-
-static void display(){
-for(String k:map.keySet()){
-int[]a=map.get(k);
-System.out.println(k+" RBC:"+a[0]+" Plasma:"+a[1]+" Platelets:"+a[2]);
+static boolean check(String g,String t,int u){
+if(t.equals("1"))return rbc.get(g)>=u;
+if(t.equals("2"))return platelets.get(g)>=u;
+return plasma.get(g)>=u;
+}
+static void reduce(String g,String t,int u){
+if(t.equals("1"))rbc.put(g,rbc.get(g)-u);
+else if(t.equals("2"))platelets.put(g,platelets.get(g)-u);
+else plasma.put(g,plasma.get(g)-u);
+save();
+}
+static void show(){
+for(String g:groups){
+System.out.println("RBC "+g+" "+rbc.get(g));
+System.out.println("Plasma "+g+" "+plasma.get(g));
+System.out.println("Platelets "+g+" "+platelets.get(g));
 }
 }
-
 static void save(){
 try{
-PrintWriter pw=new PrintWriter("inventory.txt");
-for(String k:map.keySet()){
-int[]a=map.get(k);
-pw.println(k+" "+a[0]+" "+a[1]+" "+a[2]);
+PrintWriter pw=new PrintWriter(new FileWriter("inventory.txt"));
+for(String g:groups){
+pw.println(g+" "+rbc.get(g)+" "+plasma.get(g)+" "+platelets.get(g));
 }
 pw.close();
 }catch(Exception e){}
 }
-
 static void load(){
 try{
-File f=new File("inventory.txt");
-if(!f.exists())return;
-Scanner sc=new Scanner(f);
-while(sc.hasNext()){
-String k=sc.next();
-int r=sc.nextInt(),p=sc.nextInt(),pl=sc.nextInt();
-map.put(k,new int[]{r,p,pl});
+BufferedReader br=new BufferedReader(new FileReader("inventory.txt"));
+String s;
+while((s=br.readLine())!=null){
+String[]p=s.split(" ");
+rbc.put(p[0],Integer.parseInt(p[1]));
+plasma.put(p[0],Integer.parseInt(p[2]));
+platelets.put(p[0],Integer.parseInt(p[3]));
 }
+br.close();
 }catch(Exception e){}
 }
 }
